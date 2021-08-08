56471699d025ee0a3698af785031fea2335bef79
kayheypark <kayheypark@gmail.com>
2021-08-08 23:34:28 +0900

    로그인 전역변수 작성

--- website/admin/public/assets/js/default.js
+++ website/admin/public/assets/js/default.js
@@ -1,12 +1,32 @@
 ﻿console.log('default.js');
 
-//로그인
+window.onload = function() {
+
+    fnCheckLoginCookie();
+
+};
+
+//로그인정보 변수
+var loginInfo = {
+    isLogined: false,
+    token: '',
+    memberSeq: -1
+};
+
+//로그인 쿠키 체크
+function fnCheckLoginCookie() {
+    console.log('function fnCheckLoginCookie Started');
+};
+
+//쿠키체크
+
+//로그인하기
 function fnLogin() {
     document.location.href = '/main';
     return false;
 };
 
-//로그아웃
+//로그아웃하기
 function fnLogout() {
     document.location.href = '/';
 };
