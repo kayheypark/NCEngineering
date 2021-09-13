acd7f267b8a02247f05565588b9f026b4e60d4ab
kayheypark <kayheypark@gmail.com>
2021-09-14 00:52:07 +0900

    프론트 메인 기초 퍼블리싱 및 레이아웃 분기

--- website/front/front.js
+++ website/front/front.js
@@ -3,14 +3,16 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const front = express();
 require('../../library/database.js');
+const fn = require('../../library/function.js');
+
 
 // Body Parser
 front.use(express.urlencoded({extended: false}));
 front.use(express.json());
 
 // Static Files
-front.use('/assets', express.static('./public/assets'));
-front.use('/vendor', express.static('./public/vendor'));
+front.use('partials', express.static(__dirname + '/views/partials'));
+front.use('/assets', express.static(__dirname + '/public/vendor'));
 
 // Set Templating Engine
 front.use(expressLayouts);
@@ -20,32 +22,30 @@ front.set("layout extractScripts", true);
 front.set("layout extractStyles", true);
 front.set('view engine', 'ejs');
 
+let siteInfo = {
+};
+
+/************************************************************
+   Return Functions for EJS
+************************************************************/
+
+siteInfo["isNullOrEmpty"] = function(str) {
+    return fn.isNullOrEmpty(str);
+};
+siteInfo["textWithoutHTML"] = function(str) {
+    return fn.textWithoutHTML(str);
+};
+siteInfo["numberWithComma"] = function(str) {
+    return fn.numberWithComma(str);
+};
+
 //Page List
 front.get('/', (req, res) => {
-    res.render('index', {pagename: '메인'});
+    res.render('main', {pagename:'메인'});
 });
-front.get('/about', (req, res) => {
-    res.render('about');
-});
+
 
 //Api List
-front.get('', (req, res) => {
-    global.pool.getConnection((err, connection) => {
-        if (err) throw err;
-        console.log(`connected as id ${connection.threadId}`);
 
-        connection.query('SELECT * FROM product', (err, rows) => {
-            connection.release(); // return the connection to pool
-
-            if (!err) {
-                res.send(rows);
-            } else {
-                console.log(err);
-            };
-
-        });
-
-    });
-});
 
 module.exports = front;
